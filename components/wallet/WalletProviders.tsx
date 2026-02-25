45be7c73df472ff253ae7a28106bfd8c20980837
edlakin04 <edlakin2004@gmail.com>
2026-02-25 22:13:29 +0000

    Create WalletProviders.tsx

---
+++ components/wallet/WalletProviders.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useMemo } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  BackpackWalletAdapter,
+  GlowWalletAdapter,
+  CoinbaseWalletAdapter,
+  TrustWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+
+export default function WalletProviders({ children }: { children: React.ReactNode }) {
+  const endpoint = useMemo(() => clusterApiUrl("mainnet-beta"), []);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new BackpackWalletAdapter(),
+      new GlowWalletAdapter(),
+      new CoinbaseWalletAdapter(),
+      new TrustWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
