ed99d2d23833319d9a5a4d2597f282d76ce6dc53
edlakin04 <edlakin2004@gmail.com>
2026-02-25 14:12:22 +0000

    Update page.tsx

--- app/coins/page.tsx
+++ app/coins/page.tsx
@@ -3,11 +3,6 @@ import CoinTabs from "@/components/feeds/CoinTabs";
 export default function CoinsPage() {
   return (
     <div className="fullbleed">
-      <div className="coins-title">
-        <h1>Coins</h1>
-        <p>Filtered and verified dev coins.</p>
-      </div>
-
       <CoinTabs />
     </div>
   );
