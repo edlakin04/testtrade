05c4106062569eed503f0d98976577129ae18098
edlakin04 <edlakin2004@gmail.com>
2026-02-25 18:12:39 +0000

    Update Subnav.tsx

--- components/site/Subnav.tsx
+++ components/site/Subnav.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 
 const links = [
   { href: "/coins", label: "Coins" },
+  { href: "/devs", label: "Devs" },
   { href: "/dev/apply", label: "Dev Signup" },
   { href: "/account", label: "Account" },
   { href: "/how-it-works", label: "How it works" }
