18ebe6ef8f1d7e91cad19843b1bb03bf00915791
edlakin04 <edlakin2004@gmail.com>
2026-02-25 01:56:15 +0000

    Create Header.tsx

---
+++ components/site/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="header-inner">
+      <div className="brand">
+        <span>AuthSwap</span>
+      </div>
+      <div className="badge">Solana • filtered + verified</div>
+    </div>
+  );
+}
