9129ecb04a35924d0b690320cb2cc3a4c54f1946
edlakin04 <edlakin2004@gmail.com>
2026-02-25 13:56:36 +0000

    Update Header.tsx

--- components/site/Header.tsx
+++ components/site/Header.tsx
@@ -1,10 +1,7 @@
 export default function Header() {
   return (
     <div className="header-inner header-centered">
-      <div style={{ textAlign: "center" }}>
-        <div className="brand">AuthSwap</div>
-        <div className="brand-sub">Filtered coins + verified dev listings</div>
-      </div>
+      <div className="brand">AuthSwap</div>
     </div>
   );
 }
